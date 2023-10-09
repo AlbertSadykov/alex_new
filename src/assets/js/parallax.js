a8e675cf98f35f65339e76e290ce4fba357aad60
Albert Sadykov <31353497+AlbertSadykov@users.noreply.github.com>
2023-10-09 20:57:37 +0600

    одновременный параллакс на >991

--- src/assets/js/parallax.js
+++ src/assets/js/parallax.js
@@ -25,30 +25,34 @@ window.addEventListener('scroll', handleScroll);
 document.addEventListener('DOMContentLoaded', function() {
   var machine = document.querySelector('.machine');
   var gradient = document.querySelector('.gradient');
-  
-  window.addEventListener('scroll', function() {
+  var thread = document.querySelector('.thread');
+  var ctaSection = document.querySelector('.section_cta');
+
+  function handleScroll() {
     var scrollY = window.scrollY;
     var translateY = Math.min(scrollY, 100);
+
     machine.style.backgroundPosition = '0 ' + (280 - translateY) + 'px';
+    thread.style.transform = 'translateY(' + (-translateY) + 'px)';
 
     if (scrollY >= 50) {
       gradient.style.opacity = '1';
     } else {
       gradient.style.opacity = '0';
     }
-  });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-  var thread = document.querySelector('.thread');
-  
-  window.addEventListener('scroll', function() {
-    var scrollY = window.scrollY;
-    var translateY = Math.min(scrollY, 100);
-    thread.style.transform = 'translateY(' + (-translateY) + 'px)';
-  });
-});
+    if (window.innerWidth < 991) {
+      if (scrollY === 0) {
+        ctaSection.style.marginTop = '0';
+      } else {
+        ctaSection.style.marginTop = '-280px';
+      }
+    }
+  }
 
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('resize', handleScroll); // Добавляем обработчик изменения размера окна
+});
 
 
 
